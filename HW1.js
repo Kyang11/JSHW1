dcc42e75e1b6af89224042d081e56969c5e970e9
Ku Yang <Kum.yaj@gmail.com>
2022-05-19 13:16:07 -0500

    JS firstHW

---
+++ HW1.js
@@ -0,0 +1,42 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(){
+    //Your code goes here
+    for (let i =0; i < dog_names.length; i++){
+        console.log(dog_names[i])
+}
+}
+
+//Call method here with parameters
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+// Given 
+
+let name_of_list = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    //code goes here
+    // for loop to get all name
+    // and replace with even index
+    for (let i =0; i < name_of_list.length; i++) {
+        if(i%2 ==0){
+            name_of_list.splice(i,1, "even index");
+        }
+    }
+    return name_of_list
+}
+console.log(replaceEvens())
+
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
---
+++ HW2.js
@@ -0,0 +1,91 @@
+//==========Exercise #1 ===========//
+/*
+Write a function that parses through the below object and displays all of their
+favorite food dishes as shown:
+*/
+
+let person3 = {
+    pizza:["Deep Dish","South Side Thin Crust"],
+    tacos:"Anything not from Taco bell",
+    burgers:"Portillos Burgers",
+    ice_cream:["Chocolate","Vanilla","Oreo"],
+    shakes:[{
+        oberwise:"Chocolate",
+        dunkin:"Vanilla",
+        culvers:"All of them",
+        mcDonalds:"Sham-rock-shake",
+        cupids_candies:"Chocolate Malt"
+    }]
+}
+
+
+for (let key of Object.keys(person3)){
+    console.log(`${key}=> ${person3.shakes[key]}`);
+}
+
+// or 
+console.log(person3.pizza)
+console.log(person3.tacos)
+console.log(person3.burgers)
+console.log(person3.ice_cream)
+console.log(person3.shakes[0].oberwise)
+console.log(person3.shakes[0].dunkin)
+console.log(person3.shakes[0].culvers)
+console.log(person3.shakes[0].mcDonalds)
+console.log(person3.shakes[0].cupids_candies)
+
+
+
+//=======Exercise #2=========//
+/*
+Write an object prototype for a Person that has a name and age, has a
+printInfo method, and also has a method that 
+increments the persons age by 1 each time the method is called.
+Create two people using the 'new' keyword, and print 
+both of their infos and increment one persons
+age by 3 years. Use an arrow function for both methods
+*/
+
+// Create our Person Prototype
+ function Person(name, age){
+     this.name = name;
+     this.age = age;
+     // method prototype
+    this.printInfo = function(){
+        console.log(`Name: ${this.name} \nAge: ${this.age}`)
+    }
+ }
+let man = new Person("Nathan", 25);
+
+console.table(man);
+
+console.log(man.name="Cashton")
+console.log(man)
+
+man.age=30
+console.log(man)
+
+
+// =============Exercise #3 ============//
+/*
+
+    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
+
+let c = new Promise((resolve, reject) =>{
+    let word_string = "This is the test for promising"
+    if (word_string.length > 10){
+        resolve("Big Word")
+    }else{
+        reject("Small Number")
+    }
+})
+
+c.then((message) =>{
+    console.log("This is a "+message)
+}).catch((message) =>{
+    console.log("This is a "+message)
+})
+
